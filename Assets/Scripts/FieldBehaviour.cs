0decba9f06a61c2460ba077dfd736a2a0ff46c01
Fruitlobster <altenburg.florian@web.de>
2019-11-08 22:57:21 +0100

    added small grid on click

--- Assets/Scripts/FieldBehaviour.cs
+++ Assets/Scripts/FieldBehaviour.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class FieldBehaviour : MonoBehaviour
 {
+    public GameObject smallgrid;
+    public GameObject smallgridInstance;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    //Camera.main.ScreenPointToRay(Input.mousePosition)
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        // OnMouseDown
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(smallgridInstance == null)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
+                if (hit.collider != null && hit.collider.tag == "SudokuField")
+                {
+                    smallgridInstance = Instantiate(smallgrid, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x + 0.75f, Camera.main.ScreenToWorldPoint(Input.mousePosition).y - 0.75f, 0), Quaternion.identity);
+                }
+            }
+            else
+            {
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
+                if (hit.collider != null && hit.collider.tag == "SelectiveNumber")
+                {
+                    //smallgridInstance = Instantiate(smallgrid, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x + 0.75f, Camera.main.ScreenToWorldPoint(Input.mousePosition).y - 0.75f, 0), Quaternion.identity);
+
+                }
+                Destroy(smallgridInstance);
+            }
+
+        }
     }
     //Event on Mouse Down
     void onMouseDown()
     {
-        Debug.Log("Click OK!");
+
     }
 }
