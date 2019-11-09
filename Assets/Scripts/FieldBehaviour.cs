1a1f99d38425bc18704606b59a14f1fb5e328ab2
Florian <altenburg.florian@web.de>
2019-11-09 19:55:17 +0100

    Better Small Grid Interaction

--- Assets/Scripts/FieldBehaviour.cs
+++ Assets/Scripts/FieldBehaviour.cs
@@ -6,6 +6,7 @@ public class FieldBehaviour : MonoBehaviour
 {
     public GameObject smallgrid;
     public GameObject smallgridInstance;
+    public Vector2Int mypos;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,17 +37,54 @@ public class FieldBehaviour : MonoBehaviour
                 RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
                 if (hit.collider != null && hit.collider.tag == "SelectiveNumber")
                 {
-                    //smallgridInstance = Instantiate(smallgrid, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x + 0.75f, Camera.main.ScreenToWorldPoint(Input.mousePosition).y - 0.75f, 0), Quaternion.identity);
-
+                    int value = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().Set_number(mypos, hit.collider.GetComponent<SelectiveBehavior>().mynumber);
+                    ChangeSprite(value);
+                    Destroy(smallgridInstance);
                 }
-                Destroy(smallgridInstance);
+                else
+                {
+                    Destroy(smallgridInstance);
+                }
+                
             }
 
         }
     }
     //Event on Mouse Down
-    void onMouseDown()
+    void ChangeSprite(int number)
     {
-
+        switch (number)
+        {
+            case 1:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().one;
+                break;
+            case 2:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().two;
+                break;
+            case 3:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().three;
+                break;
+            case 4:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().four;
+                break;
+            case 5:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().five;
+                break;
+            case 6:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().six;
+                break;
+            case 7:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().seven;
+                break;
+            case 8:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().eight;
+                break;
+            case 9:
+                GetComponent<SpriteRenderer>().sprite = transform.parent.parent.gameObject.GetComponent<Sudoku_Quiz>().nine;
+                break;
+            default:
+                GetComponent<SpriteRenderer>().sprite = null;
+                break;
+        }
     }
 }
--- Assets/Scripts/Sudoku_Quiz.cs
+++ Assets/Scripts/Sudoku_Quiz.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Sudoku_Quiz : MonoBehaviour
 {
+    public Sprite one;
+    public Sprite two;
+    public Sprite three;
+    public Sprite four;
+    public Sprite five;
+    public Sprite six;
+    public Sprite seven;
+    public Sprite eight;
+    public Sprite nine;
     // the numbers array is the grid of the sudoku with the following mapping position in sequence = position on grid
     // 1 = top left ; 2 = top middle ; 3 = top right ; 4 = middle left etc.
-    int[] numbers = new int[9];
+    int[,] numbers = new int[9, 9];
     // Start is called before the first frame update
     void Start()
     {
@@ -19,15 +28,18 @@ public class Sudoku_Quiz : MonoBehaviour
         
     }
 
-    void Set_number(int pos,int value)
+    public int Set_number(Vector2Int pos,int value)
     {
-        if(numbers[pos] == value)
+        if(numbers[pos.x, pos.y] == value)
         {
-            numbers[pos] = 0;
+            numbers[pos.x, pos.y] = 0;
+            Debug.Log(numbers[pos.x, pos.y]);
+            return 0;
         }
         else
         {
-            numbers[pos] = value;
+            numbers[pos.x, pos.y] = value;
+            return value;
         }
     }
 }
